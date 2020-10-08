7a4d6c2c1833be026ef3cf55c4c13233a14e863b
kassu11 <61124141+kassu11@users.noreply.github.com>
2020-10-08 22:55:16 +0300

    Aloitus

---
+++ code.js
@@ -0,0 +1,17 @@
+document.getElementsByClassName("language-html")[0].textContent = 
+`<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Epic sivusto</title>
+  </head>
+  <body>
+    <h1>Otsikko</h1>
+    <div id = "menu">
+      <div>Idk</div>
+    </div>
+  </body>
+</html>
+
+<script src = "code.js"></script>`
